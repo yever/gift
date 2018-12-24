6e692fe6c5aa9a3f54e8f2193f741d7f9f9d8207
Ya'ar Hever <yaarhever@gmail.com>
2018-12-24 23:34:41 +0100

    Rearrange code and parse width & height (#3)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
 #[macro_use]
 extern crate nom;
 
-mod parser;
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum GIFVersion {
+    GIF89a,
+    GIF87a,
+}
 
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct GIF {
+    pub version: GIFVersion,
+    pub width: u16,
+    pub height: u16,
+}
+
+pub mod parser;
+
+pub use self::parser::parse_gif;
